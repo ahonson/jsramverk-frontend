e4e313c783497562a4951c498ef1dc0b0b197de4
ahonson ahonson <ahonson@gmail.com>
2020-09-05 22:58:40 +0100

    Basic me-page with two routes in Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ReportsComponent } from './reports/reports.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReportsComponent,
+    AboutMeComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'about', component: AboutMeComponent},
+      {path: 'reports/week/1', component: ReportsComponent},
+      {path: '', redirectTo: '/about', pathMatch: 'full'},
+      {path: '**', component: PageNotFoundComponent} // this route has to come last
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
